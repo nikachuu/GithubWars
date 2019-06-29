26da80338aef15dd49dcbd70d83d45b70f6dbe67
Febatista107 <febatista107@gmail.com>
2019-06-29 16:47:13 -0300

    Fetching users

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const API_URL = "https://api.github.com";
+// const API_URL = "https://api.github.com";
+const API_URL = "http://localhost:8080";
 
 function getUserData(username) {
-  return fetch(`${API_URL}/users/${username}`).then(result => result.json());
+  // return fetch(`${API_URL}/users/${username}`).then(result => result.json());
+  return fetch(`${username}.json`).then(result => result.json());
 }
 
-getUserData("Febatista107").then(user => {
-  console.log(user);
+const firstGitInput = document.querySelector("input[name=firstGitInput]");
+const secondGitInput = document.querySelector("input[name=secondGitInput]");
+const submitForm = document.querySelector("input[name=submitForm]");
+
+submitForm.addEventListener("click", async event => {
+  event.preventDefault();
+  console.log("Pega dados");
+  try {
+    const firstUser = await getUserData(firstGitInput.value);
+    const secondUser = await getUserData(secondGitInput.value);
+    console.log(firstUser, secondUser);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
